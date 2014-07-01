cfe4f8258403b60ea20cf77bc71bfd6fcccbe786
lutielo <lutielo@hotmail.com>
2014-07-01 18:36:14 -0300

    Criação do filtro de Admin

---
+++ projeto-progweb/src/main/java/br/unisul/progweb/filtro/AdminFilter.java
@@ -0,0 +1,47 @@
+package br.unisul.progweb.filtro;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import br.unisul.progweb.bean.Usuario;
+
+@WebFilter(filterName = "AdminFilter", urlPatterns = "/admin/*")
+public class AdminFilter implements Filter {
+
+	public AdminFilter() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpSession session = req.getSession();
+		
+		Usuario usuario = (Usuario) session.getAttribute("usuario");
+		if(1 == usuario.getPerfil().getCdperfil()) {
+			chain.doFilter(request, response);
+		} else {
+			session.setAttribute("msg","Você não está logado no sistema!");
+
+            ((HttpServletResponse)response).sendRedirect("../home.jsp");
+		}
+	}
+
+	public void destroy() {
+
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+
+	}
+
+}
